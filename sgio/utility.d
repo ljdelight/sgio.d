c5d458e46da61e14449d5a426e5428b093b090a4
Lucas Burson <ljdelight@gmail.com>
2014-10-17 19:30:02 -0500

    Add new utility method bufferGetString. This utility simplifies getting X bytes from a buffer which might not be null-terminated.

--- sgio/utility.d
+++ sgio/utility.d
@@ -1,8 +1,9 @@
 
 module sgio.utility;
 
-import std.string : format;
+import std.string : format, strip;
 import std.xml : isChar;
+import std.conv;
 
 /**
  * Decode the byte in the buffer given the offset and bitmask.
@@ -29,6 +30,39 @@ body
    return res;
 }
 
+/**
+ * Get a string from buffer where the string spans [offset_start, offset_end).
+ * Params:
+ *    buffer = Buffer with an ASCII string to obtain.
+ *    offset_start = Beginning byte offset within the buffer where the string starts.
+ *    offset_end = Ending byte offset which is not included in the string.
+ */
+string bufferGetString(ubyte[] buffer, ulong offset_start, ulong offset_end)
+in
+{
+   assert(buffer != null);
+   assert(offset_start < offset_end);
+   assert(offset_end <= buffer.length);
+}
+body
+{
+   ulong bufflen = offset_end - offset_start;
+
+   // add one to the lenth for null-termination
+   ubyte[] temp = new ubyte[bufflen+1];
+   temp[0..bufflen] = buffer[offset_start..offset_end];
+   temp[bufflen] = '\0';
+
+   return strip(to!string(cast(const char*) temp.ptr));
+}
+
+unittest
+{
+   ubyte[] no_null = [' ', 'A', 'B', 'C', ' '];
+   assert("ABC" == bufferGetString(no_null, 0, no_null.length));
+   assert("ABC" == bufferGetString(no_null, 1, no_null.length-1));
+   assert("A" == bufferGetString(no_null, 1, 2));
+}
 
 string writeBuffer(ubyte[] buff, ulong length)
 {
