59ee16e39faac813fd373e51c1fda14061d8857c
Lucas Burson <ljdelight@gmail.com>
2014-10-27 00:48:06 -0500

    Cleanup/update of SCSICheckConditionException class: Updated member variables (added properties, "m_" prefix) and now using a better constructor (using enum for contants, using decodeByte(), and throwing SCSIException when the response code doesn't make sense).

--- sgio/exceptions.d
+++ sgio/exceptions.d
@@ -1,6 +1,6 @@
 
 module sgio.exceptions;
-
+import sgio.utility;
 import std.string : format;
 
 class SCSIException : Exception
@@ -19,48 +19,80 @@ class IoctlFailException : SCSIException
    }
 }
 
+
 class SCSICheckConditionException : SCSIException
 {
-   static int SENSE_FORMAT_CURRENT_FIXED = 0x70;
-
-   ubyte valid;
-   ubyte response_code;
-   ubyte filemark;
-   ubyte eom;
-   ubyte ili;
-   ubyte sdat_ovfl;
-   ubyte sense_key;
-   ubyte[4] information;
-   ubyte additional_sense_length;
-   ubyte[4] command_specific_information;
-   ubyte additional_sense_code;
-   ubyte additional_sense_code_qualifier;
-   ubyte field_replaceable_unit_code;
-   int ascq;
-
    this(ubyte[] sense)
    {
-      valid = sense[0] & 0x80;
-      response_code = sense[0] & 0x7f;
+      // SPC 4.5.3 Fixed format sense data
+      m_valid = decodeByte(sense, 0, 0x80);
+      m_response_code = decodeByte(sense, 0, 0x7f);
 
-      if (response_code == SENSE_FORMAT_CURRENT_FIXED)
+      if (m_response_code != SenseFormat.CURRENT_INFO_FIXED &&
+          m_response_code != SenseFormat.DEFERRED_ERR_FIXED)
       {
-         filemark = sense[2] & 0x80;
-         eom = sense[2] & 0x40;
-         ili = sense[2] & 0x20;
-         sdat_ovfl = sense[2] & 0x10;
-         sense_key = sense[2] & 0x0f;
-         information = sense[3..7];
-         additional_sense_length = sense[7];
-         command_specific_information = sense[8..12];
-         additional_sense_code = sense[12];
-         additional_sense_code_qualifier = sense[13];
-         field_replaceable_unit_code = sense[14];
+         throw new SCSIException(format("SCSICheckConditionException was thrown but"
+            ~ "response_code (0x%02x) doesn't imply fixed-format sense."
+            ~ " Raising SCSIException instead", m_response_code));
       }
-      ascq = additional_sense_code << 8 + additional_sense_code_qualifier;
 
-      super(format("Check condition raised. ASC+Q=0x%04x", ascq));
+      m_filemark  = decodeByte(sense, 2, 0x80);
+      m_eom       = decodeByte(sense, 2, 0x40);
+      m_ili       = decodeByte(sense, 2, 0x20);
+      m_sdat_ovfl = decodeByte(sense, 2, 0x10);
+      m_sense_key = decodeByte(sense, 2, 0x0f);
+      m_information                  = sense[3..7];
+      m_additional_sense_length      = sense[7];
+      m_command_specific_information = sense[8..12];
+      m_additional_sense_code        = sense[12];
+      m_additional_sense_code_qualifier = sense[13];
+      m_field_replaceable_unit_code  = sense[14];
+      m_ascq = m_additional_sense_code << 8 + m_additional_sense_code_qualifier;
+
+      super(format("Check condition raised. ASC+Q=0x%04x", m_ascq));
    }
+
+   enum SenseFormat: int
+   {
+      CURRENT_INFO_FIXED = 0x70,
+      DEFERRED_ERR_FIXED = 0x71,
+      CURRENT_INFO_DESCR = 0x72,
+      DEFERRED_ERR_DESCR = 0x73
+   }
+
+   @property
+   {
+      ubyte valid() { return m_valid; }
+      ubyte response_code() { return m_response_code; }
+      ubyte filemark() { return m_filemark; }
+      ubyte eom() { return m_eom; }
+      ubyte ili() { return m_ili; }
+      ubyte sdat_ovfl() { return m_sdat_ovfl; }
+      ubyte sense_key() { return m_sense_key; }
+      ubyte[4] information() { return m_information; }
+      ubyte additional_sense_length() { return m_additional_sense_length; }
+      ubyte[4] command_specific_information() { return m_command_specific_information; }
+      ubyte additional_sense_code() { return m_additional_sense_code; }
+      ubyte additional_sense_code_qualifier() { return m_additional_sense_code_qualifier; }
+      ubyte field_replaceable_unit_code() { return m_field_replaceable_unit_code; }
+      int ascq() { return m_ascq; }
+   }
+
+private:
+   ubyte m_valid;
+   ubyte m_response_code;
+   ubyte m_filemark;
+   ubyte m_eom;
+   ubyte m_ili;
+   ubyte m_sdat_ovfl;
+   ubyte m_sense_key;
+   ubyte[4] m_information;
+   ubyte m_additional_sense_length;
+   ubyte[4] m_command_specific_information;
+   ubyte m_additional_sense_code;
+   ubyte m_additional_sense_code_qualifier;
+   ubyte m_field_replaceable_unit_code;
+   int m_ascq;
 }
 
 class BadOpCodeException : SCSIException
